bfa3afb17a519ede0091415cb64b651681a43acb
sᴇᴜʟᴊɴ <36707871+seulJN@users.noreply.github.com>
2023-04-14 11:28:28 -0400

    Create BJP4 Exercise 9.10: DiscountBill.java

---
+++ BJP4 Exercise 9.10: DiscountBill.java
@@ -0,0 +1,42 @@
+public class DiscountBill extends GroceryBill{
+    private int itemsDiscounted;
+    private double discounted;
+    private boolean prefCustomer;
+    
+    public DiscountBill(Employee clerk, boolean preferred){
+        super(clerk);
+        itemsDiscounted = 0;
+        discounted = 0.0;
+        prefCustomer = preferred;
+    }
+    
+    public void add(Item i){
+        super.add(i);
+        if (prefCustomer && i.getDiscount() > 0)
+        {
+            itemsDiscounted++;
+            discounted += i.getDiscount();
+        }
+     }
+    
+    public double getTotal()
+    {
+        return super.getTotal() - discounted;
+    }
+    
+    public int getDiscountCount()
+    {
+        return itemsDiscounted;
+    }
+    
+    public double getDiscountAmount()
+    {
+        return discounted;
+    }
+    
+    public double getDiscountPercent()
+    {
+       return discounted * 100 / super.getTotal();
+    }
+    
+}
