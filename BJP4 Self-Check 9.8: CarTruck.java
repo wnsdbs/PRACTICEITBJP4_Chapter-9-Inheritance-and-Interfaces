15d939279bda08648977cec9581a1201a4fe1919
sᴇᴜʟᴊɴ <36707871+seulJN@users.noreply.github.com>
2023-04-14 11:15:24 -0400

    Update BJP4 Self-Check 9.8: CarTruck.java

--- BJP4 Self-Check 9.8: CarTruck.java
+++ BJP4 Self-Check 9.8: CarTruck.java
@@ -27,9 +27,9 @@ What is the output from the following statements?
         
                                         OUTPUT:
 System.out.println(mycar);              [vroom]
-mycar.m1();	                         [car 1]
-mycar.m2();	                         [car 2]
-System.out.println(mytruck);	     [vroom]
-mytruck.m1();	                       [truck 1]
-mytruck.m2();	                        [car 2]
+mycar.m1();	                        [car 1]
+mycar.m2();	                        [car 2]
+System.out.println(mytruck);	    [vroom]
+mytruck.m1();	                    [truck 1]
+mytruck.m2();	                    [car 2]
  
