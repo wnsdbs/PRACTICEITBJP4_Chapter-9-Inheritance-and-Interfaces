1fec8807953d7b865f329c6d51048172cb52f36b
sᴇᴜʟᴊɴ <36707871+seulJN@users.noreply.github.com>
2023-04-14 11:10:54 -0400

    Create BJP4 Self-Check 9.10: inheritanceVariableSyntax.java

---
+++ BJP4 Self-Check 9.10: inheritanceVariableSyntax.java
@@ -0,0 +1,16 @@
+Consider the following classes:
+
+  public class Vehicle {...}
+  public class Car extends Vehicle {...}
+  public class SUV extends Car {...}
+
+Which of the following are legal statements?
+
+a. SUV s = new Car();
+b. Vehicle v = new SUV();
+c. Car c = new Vehicle();
+d. SUV s = new SUV();
+e. Car c = new SUV();
+f. Vehicle v = new Car();
+
+Answer(s): b. Vehicle v = new SUV();, d. SUV s = new SUV();, e. Car c = new SUV();, Vehicle v = new Car();
