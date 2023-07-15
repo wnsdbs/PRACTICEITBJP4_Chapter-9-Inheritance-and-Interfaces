94258d091a6f905613ec9d40f92fa472a9af2cc4
ᴏɴᴇꜱᴇᴜʟᴏɴᴇ <36707871+oneseulone@users.noreply.github.com>
2023-07-14 23:16:20 -0400

    Update BJP4 Exercise 9.1: Marketer.java

--- BJP4 Exercise 9.1: Marketer.java
+++ BJP4 Exercise 9.1: Marketer.java
@@ -1,7 +1,9 @@
+/*
 Write the class Marketer to accompany the other law firm classes described in this chapter. 
 Marketers make $50,000 ($10,000 more than general employees) and have an additional method 
 called advertise that prints "Act now, while supplies last!" Make sure to interact with the 
 Employee superclass as appropriate.
+	*/
   
 // A class to represent employees in general.
 public class Employee {
