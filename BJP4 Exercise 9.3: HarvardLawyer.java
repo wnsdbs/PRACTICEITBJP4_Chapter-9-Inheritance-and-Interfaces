11e286bb0d9d14af2a802e70db406d556094d09f
ᴏɴᴇꜱᴇᴜʟᴏɴᴇ <36707871+oneseulone@users.noreply.github.com>
2023-07-25 14:27:54 -0400

    Update BJP4 Exercise 9.3: HarvardLawyer.java

--- BJP4 Exercise 9.3: HarvardLawyer.java
+++ BJP4 Exercise 9.3: HarvardLawyer.java
@@ -1,25 +1,29 @@
 /*
-Write a class HarvardLawyer to accompany the other law firm classes described in this chapter. 
-Harvard lawyers are like normal lawyers, but they make 20% more money than a normal lawyer, 
-they get 3 days more vacation, and they have to fill out four of the lawyer's forms to go on vacation. 
-That is, the getVacationForm method should return "pinkpinkpinkpink". Make sure to interact with 
-the superclass as appropriate.
-  */
+*	Write a class HarvardLawyer to accompany the other law firm classes described in this chapter. 
+*	Harvard lawyers are like normal lawyers, but they make 20% more money than a normal lawyer, 
+*	they get 3 days more vacation, and they have to fill out four of the lawyer's forms to go on vacation. 
+*	That is, the getVacationForm method should return "pinkpinkpinkpink". Make sure to interact with 
+*	the superclass as appropriate.
+*/
 
-SOLUTION:
+//SOLUTION:
 
-public class HarvardLawyer extends Lawyer{ 
+public class HarvardLawyer extends Lawyer
+{ 
     public double getSalary(){
         return super.getSalary() * 1.2;
     }    
+	
     public int getVacationDays(){
         return super.getVacationDays() + 3;
     }    
+	
     public String getVacationForm(){
         String temp = "";
         for (int  i = 0; i < 4; i++)
           temp += super.getVacationForm();
-        return temp;
+       
+	    return temp;
     }
 }
 
