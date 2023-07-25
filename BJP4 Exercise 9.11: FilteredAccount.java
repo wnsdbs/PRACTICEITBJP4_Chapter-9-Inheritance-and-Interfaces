ffb8d1957f24919efd9c5f129c74c4b85e67c1a6
ᴏɴᴇꜱᴇᴜʟᴏɴᴇ <36707871+oneseulone@users.noreply.github.com>
2023-07-25 14:21:48 -0400

    Update BJP4 Exercise 9.11: FilteredAccount.java

--- BJP4 Exercise 9.11: FilteredAccount.java
+++ BJP4 Exercise 9.11: FilteredAccount.java
@@ -1,19 +1,20 @@
-public class FilteredAccount extends Account {
+public class FilteredAccount extends Account 
+{
     int total = 0;
     int zero = 0;
     
-    public FilteredAccount(Client c) {
+    public FilteredAccount(Client c){
         super(c);
     }    
-    public boolean process(Transaction t) {
+    public boolean process(Transaction t){
         total++;
-        if(t.value() == 0) {
+        if(t.value() == 0){
             zero++;
             return true;
         }        
         return super.process(t);
     }    
-    public double percentFiltered() {
+    public double percentFiltered(){
         if(total == 0)
             return 0.0;
         return ((double) zero) * 100 / total;
