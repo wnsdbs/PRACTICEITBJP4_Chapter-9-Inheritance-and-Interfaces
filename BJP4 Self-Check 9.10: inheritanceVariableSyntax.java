c75488423cb307461225b42f465418dae677b5e6
ᴏɴᴇꜱᴇᴜʟᴏɴᴇ <36707871+oneseulone@users.noreply.github.com>
2023-07-25 14:30:26 -0400

    Update BJP4 Self-Check 9.10: inheritanceVariableSyntax.java

--- BJP4 Self-Check 9.10: inheritanceVariableSyntax.java
+++ BJP4 Self-Check 9.10: inheritanceVariableSyntax.java
@@ -1,16 +1,20 @@
-Consider the following classes:
+// Consider the following classes:
 
   public class Vehicle {...}
   public class Car extends Vehicle {...}
   public class SUV extends Car {...}
 
-Which of the following are legal statements?
+// Which of the following are legal statements?
 
-a. SUV s = new Car();
-b. Vehicle v = new SUV();
-c. Car c = new Vehicle();
-d. SUV s = new SUV();
-e. Car c = new SUV();
-f. Vehicle v = new Car();
+ a. SUV s = new Car();
+ b. Vehicle v = new SUV();
+ c. Car c = new Vehicle();
+ d. SUV s = new SUV();
+ e. Car c = new SUV();
+ f. Vehicle v = new Car();
 
-Answer(s): b. Vehicle v = new SUV();, d. SUV s = new SUV();, e. Car c = new SUV();, Vehicle v = new Car();
+// Answer(s): 
+  b. Vehicle v = new SUV();
+  d. SUV s = new SUV(); 
+  e. Car c = new SUV();
+  f. Vehicle v = new Car();
